e1c797d3eb722571b7fbcdf4e218419b70192ec5
revfran <2746521+revfran@users.noreply.github.com>
2020-11-15 12:56:48 +0100

    Examples with datatables

--- cypress/integration/EAapp.feature
+++ cypress/integration/EAapp.feature
@@ -1,6 +1,23 @@
 Feature: EA app
 
     @focus
-    Scenario: Logging to EA app
-        Given I login to EA app page
+    Scenario: Log in to EA app
+        Given I visit EA app page
+        When I login to EA app page with "admin" and "password"
         Then I see "Home - Execute Automation Employee App" in the title
+
+    @focus @datatable_sample
+    Scenario: Log in to EA app (datatable example)
+        Given I visit EA app page
+        When I login to EA app page with following credentials
+            | username | password |
+            | admin    | password |
+        Then I see "Home - Execute Automation Employee App" in the title
+
+    @focus
+    Scenario: Failed log in to EA app
+        Given I visit EA app page
+        When I login to EA app page with following credentials
+            | username | password |
+            | admin    | 1234     |
+        Then I see "Login" in the title
--- cypress/integration/EAapp/ea_steps.js
+++ cypress/integration/EAapp/ea_steps.js
@@ -1,9 +1,33 @@
 /// <reference types="cypress" />
 
-import { Given } from "cypress-cucumber-preprocessor/steps";
+import { Given, When } from "cypress-cucumber-preprocessor/steps";
 
-Given('I login to EA app page', () => {
+Given('I visit EA app page', () => {
     cy.visit("/");
+})
 
-    cy.login(Cypress.env("username"), Cypress.env("password"))
-  })
+When('I login to EA app page with {string} and {string}', (username, password) => {
+    cy.login(username, password)
+})
+
+When('I login to EA app page with following credentials', (datatable) => {
+
+    // Parse first row of data
+    let data = datatable.rawTable
+        .slice(1)
+        .map(
+            (row) => {
+                cy.log("Row data: ",row);
+                return {
+                    username: row[0],
+                    password: row[1]
+                }
+            }
+        );
+
+    cy.log(data[0]);
+
+    cy.login(data[0].username, data[0].password)
+})
+
+
